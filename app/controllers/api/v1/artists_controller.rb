19b39248f26ec1a0c18fcc164f18ffab2a584183
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-14 00:20:20 +0000

    Had  to configure to handle the input from the frontend better

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,5 @@
 module Api
   module V1
-
     class ArtistsController < ApplicationController
       before_action :authorize_access_request!, except: [:show, :index]
       before_action :set_artist, only: %i[ show update destroy ]
@@ -8,7 +7,6 @@ module Api
       # GET /artists
       def index
         @artists = Artist.all
-
         render json: @artists
       end
 
@@ -19,12 +17,12 @@ module Api
 
       # POST /artists
       def create
-        @artist = Artist.new(artist_params)
+        @artist = current_user.artists.build(artist_params) # Associate with current user
 
         if @artist.save
-          render json: @artist, status: :created, location: @artist
+          render json: @artist, status: :created 
         else
-          render json: @artist.errors, status: :unprocessable_content
+          render json: @artist.errors, status: :unprocessable_entity 
         end
       end
 
@@ -33,25 +31,30 @@ module Api
         if @artist.update(artist_params)
           render json: @artist
         else
-          render json: @artist.errors, status: :unprocessable_content
+          render json: @artist.errors, status: :unprocessable_entity 
         end
       end
 
       # DELETE /artists/1
       def destroy
         @artist.destroy!
+        head :no_content # Add proper response
       end
 
       private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_artist
-          @artist = Artist.find(params[:id])
-        end
 
-        # Only allow a list of trusted parameters through.
-        def artist_params
+      def set_artist
+        @artist = Artist.find(params[:id])
+      end
+
+      def artist_params
+        # Handles both nested and flat parameters
+        if params[:artist]
           params.require(:artist).permit(:name)
+        else
+          params.permit(:name)
         end
+      end
     end
   end
-end
+en
