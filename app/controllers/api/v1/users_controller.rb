c6b390267efb306ca688c5c9240b1cf0536c7eb7
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-24 16:31:13 +0000

    created a user controller in order to give the user power to update their account

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,34 @@
+module Api
+    module V1
+        class UsersController < ApplicationController
+            before_action :authorize_access_request!
+            before_action :set_user, only: [:show, :update]
+
+            #GET 
+
+            def show
+                render json: @user, except: [:password_digest]
+            end
+
+            #Patch/PUT
+
+            def update
+                if @user.update(user_params)
+                    render json: @user, except: [:password_digest]
+                else
+                    render json: { errors: @user.errors.full_message}, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def set_user
+                @user = current_user
+            end
+
+            def user_params
+                params.require(:user).permit(:username, :email, :password, :password_confirmation)
+            end
+        end
+    end
+end
