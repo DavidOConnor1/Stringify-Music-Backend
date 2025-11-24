a1d9801bc60e21cc38b355966224e8635f281552
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-24 18:59:15 +0000

    had a misconfiguration in my route

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,19 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :songs
-      resources :artists  
-
+      resources :artists
       get 'user', to: 'users#show'
       patch 'user', to: 'users#update'
-      put 'user', to: 'user#update'
+      put 'user', to: 'users#update' 
 
-      post 'upgrade_to_artist', to 'users#upgrade_to_artist'
+   
+      post 'upgrade_to_artist', to: 'users#upgrade_to_artist'
       get 'public_songs', to: 'songs#public_index'
-
     end
   end
   
   root to: "home#index"
 
-  
   post "refresh", controller: :refresh, action: :create
   post "signin", controller: :signin, action: :create
   post "signup", controller: :signup, action: :create
