8f91d0c171c109271e7436c2932f25d3d9d3e88e
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-11-24 21:09:02 +0000

    writing the logic to allow users to edit their artist name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,21 +5,28 @@ module Api
             before_action :set_user, only: [:show, :update]
 
            
-           def upgrade_to_artist
-
+          def upgrade_to_artist
             artist_name = params[:artist_name]
-
+  
             if artist_name.blank?
-                render json: {error: "Artist Name is required"}, status: :unprocessable_entity
+                render json: { error: "Artist name is required" }, status: :unprocessable_entity
                 return
             end
-
-            if current_user.update(is_artist: true)
-                render json: {message: "You became an Artist!", user: current_user}
-                else
-                    render json: {error:"Upgraded Failed"}, status: :unprocessable_entity
-                end
+  
+            # Create artist record and update user
+            artist = Artist.new(name: artist_name, user_id: current_user.id)
+  
+             if artist.save && current_user.update(is_artist: true, artist_name: artist_name)
+                render json: { 
+                    message: "You are now an artist!", 
+                    user: current_user,
+                    artist: artist
+                    }
+            else
+                errors = [artist.errors.full_messages, current_user.errors.full_messages].flatten
+                render json: { error: errors.join(', ') }, status: :unprocessable_entity
             end
+        end
            
             #GET 
             def show
@@ -29,12 +36,21 @@ module Api
             #Patch/PUT
 
             def update
-                if @user.update(user_params)
-                    render json: @user, except: [:password_digest]
+                update_params = user_params
+  
+                # Only validate password if it's being changed
+                if update_params[:password].blank?
+                    update_params.delete(:password)
+                    update_params.delete(:password_confirmation)
+                end
+  
+                if current_user.update(update_params)
+                    render json: current_user, except: [:password_digest]
                 else
-                    render json: { errors: @user.errors.full_message}, status: :unprocessable_entity
+                    render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
                 end
             end
+
 
             private
 
