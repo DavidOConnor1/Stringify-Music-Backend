7c8a6742492c618e1571e89bc017a6ad42ea266a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-12-15 16:53:45 +0000

    added the playlists to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,36 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :songs
       resources :artists
+      resources :playlists, only: [:index, :show, :create, :update, :destroy] do
+        member do
+          post 'add_song'
+          delete 'remove_song'
+        end
+      end 
 
+      #api routes
       get 'music/search', to: 'music#search'
       get 'music/featured_artists', to: "music#featured_artists"
       get 'music/featured_songs', to: "music#featured_songs"
       get 'music/new_releases', to: "music#new_releases"
 
+      #user routes
       get 'user', to: 'users#show'
       patch 'user', to: 'users#update'
       put 'user', to: 'users#update' 
 
-   
+      #become an artist routes
       post 'upgrade_to_artist', to: 'users#upgrade_to_artist'
       get 'public_songs', to: 'songs#public_index'
+
+      #user specific playlists
+      get 'user/:user_id/playlists', to: 'playlist#index'
     end
   end
-  
+  #home page route
   root to: "home#index"
 
+  #account creation routes
   post "refresh", controller: :refresh, action: :create
   post "signin", controller: :signin, action: :create
   post "signup", controller: :signup, action: :create
