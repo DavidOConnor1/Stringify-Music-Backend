faee38209011f50070cc55fdf8eacbe2e170bf77
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-12-15 16:25:37 +0000

    came back to add finishing touches to the backend

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -35,4 +35,55 @@ module Api::V1
             render json: @playlist, include: [:user, songs: {include : : artist}]
         end
 
-        
+        def create 
+            if @playlist.save
+                render json: @playlist, status: :created
+            else 
+                render json: {errors: @playlist.errors.full_messages}, status: :unprocessable_entity
+            end
+        end
+
+        #Patch/Put
+
+        def update
+            #Checks ownership
+            unless @playlist.user_id == current_user.id
+                return render json: {error: "This may not be your playlist to update"}, status: :forbidden
+            end
+
+            if @playlist.update(playlist_params)
+                render json: @playlist
+            else
+                render json: {errors: @playlist.errors.full_messages}, status: :unprocessable_entity
+            end
+        end
+
+        #delete
+        def destroy
+            #checks ownership of playlist
+            unless @playlist.user_id == current_user.id
+                return render json: {error: "You are not the owner to remove this playlist"}, status: :forbidden
+            end
+
+            @playlist.destroy
+            render json: {message :"Playlist has been deleted successfully"}
+        end
+
+        def add_song
+            #clarify ownership
+            unless @playlist.user_id == current_user.id
+                return render json: {error: "Cannot add music to this playlist because you are not the owner."},
+                status: :forbidden
+            end
+
+            song = Song.find_by(id: params[:song_id])
+            unless song
+                return render json: {error: "Song not found"}, status: not_found
+                end
+
+                #checks if song is already in playlist
+                if @playlist.songs.exists?(song.id)
+                    return render json: {error: "song already in playlist"},
+                    
+
+            
