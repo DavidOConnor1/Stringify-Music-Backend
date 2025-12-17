23421439e0ef0c34037d6ea13f92c41eb9485d19
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2025-12-17 11:07:49 +0000

    preparing for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,8 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  #Prod setup
+  config.public_file_server.enabled = true
+
 end
